0fc4cefb473e7e3bea04feee11d9227d2610b706
kimberly.coston <kimberly.coston@yahoo.com>
2025-02-02 16:53:29 -0500

    updated portfolio

---
+++ src/components/connectdots/connectdots.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./connectdots.css"; // Create this CSS file for styling
+
+const About = () => {
+  return (
+    <>
+      {/* Connect Dots Section */}
+      <section id="about-second" className="about-second-section">
+  <div className="about-title-container">
+    <h1 className="about-title2 text-outline">CONNECTING<br />THE DOTS</h1>
+  </div>
+  <div className="about-title-container">
+    <h1 className="about-title3">BETWEEN<br /> NURSING<br /> AND INNOVATION</h1>
+  </div>
+</section>
+
+    </>
+  );
+};
+
+export default About;
